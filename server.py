e09aaf956eb112c5316c6259e33f9f6cb9cccc08
zarak3268 <zarak3268@gmail.com>
2025-10-03 22:28:55 -0400

    Basic flask app

---
+++ server.py
@@ -0,0 +1,11 @@
+import cv2
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World!\n"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", use_reloader=False)
