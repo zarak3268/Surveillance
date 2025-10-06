4ade6d8eb669a3a1494c1ed00cd61d7aceab8310
Zarak <zarak3268@gmail.com>
2025-10-05 21:38:53 -0400

    Basic video streaming

--- server.py
+++ server.py
@@ -1,11 +1,31 @@
 import cv2
-from flask import Flask
+from flask import Flask, Response
+from picamera2 import Picamera2, Preview
+import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello World!\n"
+
+def video_stream():
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480)}))
+    picam2.start()
+
+    while picam2.started:
+        frame = picam2.capture_array()
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    
+    picam2.stop()
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", use_reloader=False)
